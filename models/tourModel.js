e3c4642189635dfe126b43cc3b3076be7f750dc1
marcinzygan <soul@o2.pl>
2023-06-30 08:21:29 +0200

    virtual populate for reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -118,6 +118,12 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+//Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
 //Document middleware : runs before save() and create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
