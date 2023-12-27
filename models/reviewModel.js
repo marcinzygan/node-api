4e1a2d3a2bb0ddc7f300ee137d45aebd04ee86d0
marcinzygan <soul@o2.pl>
2023-12-27 13:37:54 +0100

    calculating average rating for reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,6 @@
 // review , rating , createdAt , ref to tour / ref to user .
 const mongoose = require('mongoose');
-
+const Tour = require('./tourModel');
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -34,6 +34,7 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
@@ -41,5 +42,42 @@ reviewSchema.pre(/^find/, function (next) {
   });
   next();
 });
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: `$tour`,
+        nRating: { $sum: 1 },
+        avgRating: { $avg: `$rating` },
+      },
+    },
+  ]);
+  console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post(`save`, function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.review = await this.findOne();
+  console.log(this.review);
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.review.constructor.calcAverageRatings(this.review.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
